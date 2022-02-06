8af2f68c6b078396c0658cf8251a7327f778a1d4
Ashraful <ashrafulislam2880@gmail.com>
2022-02-06 20:09:00 +0600

    Explore Page Design Improved

--- src/Page/Explore/Explore.js
+++ src/Page/Explore/Explore.js
@@ -6,7 +6,8 @@ const Explore = () => {
     const [cars]=useServices();
     return (
         <div>
-            <h2 className="text-danger pt-4 mb-5 fw-bold"> Awesome Car Collections </h2>
+            <h2 className="text-danger pt-4 fw-bold"> Awesome Car Collections </h2>
+            <p className="mb-5 text-secondary fw-bold"> We are The Leading Auto Car Company With <br /> The World-Wide Reputation </p>
             <div  className="container row mx-auto mb-4">
                 {
                     cars.map((car)=><Car key={car._id} car={car}/>)
