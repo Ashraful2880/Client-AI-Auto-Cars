1c87667a8d1891ddfcb89feec73c767a31c5c3cd
Ashraful <ashrafulislam2880@gmail.com>
2022-02-08 12:45:29 +0600

    Explore Page Design Improved

--- src/Page/Car/Car.js
+++ src/Page/Car/Car.js
@@ -3,34 +3,24 @@ import { Link } from 'react-router-dom';
 import './Car.css';
 
 const Car = ({car}) => {
-    const{url,name,_id,price,model,modelYear,type,gear,fuel,details}=car;
+    const{url,name,_id,price,model,modelYear,type,gear,fuel}=car;
     return (
         <div className="col-md-6 col-sm-12 col-lg-4 mx-auto position-relative text-start mb-4 car-container rounded-3" data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-sine">
-            <img className="w-100 border rounded-3 car-image" src={url} alt="Car-Imag"/>
-            <div className="mt-4 mx-auto">
-                <h4 className="ms-2 my-3 d-inline fw-bold">{name} ({model})</h4>
-            </div>
-            <div className="mt-2 d-flex">
-                <h6 className="ms-2 bg-secondary p-2 rounded-3 text-light text-center">
-                    <i className="fas fa-clock"></i> {modelYear}
-                </h6>
-                <h6 className="ms-2 bg-success p-2 rounded-3 text-light text-center">
-                    <i className="fas fa-cogs"></i> {gear} 
-                </h6>
-                <h6 className="ms-2 bg-secondary p-2 rounded-3 text-light text-center">
-                    <i className="fas fa-gas-pump"></i>  {fuel}
-                </h6>
-                <h6 className="ms-2 bg-success p-2 rounded-3 text-light text-center">
-                    <i className="fas fa-car"></i> {type}
-                </h6>
-            </div>
-                <h5 className="ms-2 mb-2 text-danger"> Price: ${price}.00</h5>
-                <div className="ms-2 text-secondary">
-                   <p>{details}</p>
-                </div>
             <Link className="text-dark text-decoration-none" to={`/purchase/${_id}`} >
-                <button className="btn btn-primary mb-4 ms-2 rounded-3"> Book Now </button>
+                <img className="w-100 border rounded-3 car-image position-relative" src={url} alt="Car-Imag"/>
             </Link>
+            <p className="ms-2 mb-2 text-light bg-danger py-1 px-2 rounded-3 fw-bold price"> Price: ${price}</p>
+            <div className="d-flex justify-content-between border rounded-2 details-area">
+                <h6 className="p-2 text-light ps-3 text-center mb-0"> {modelYear} </h6>
+                <h6 className="p-2 text-light ps-3 text-center mb-0"> {gear} </h6>
+                <h6 className="p-2 text-light ps-3 text-center mb-0"> {fuel} </h6>
+                <h6 className="p-2 text-light ps-3 text-center mb-0"> {type} </h6>
+            </div>
+            <div className="mb-4 mt-2">
+                <Link className="text-dark text-decoration-none" to={`/purchase/${_id}`} >
+                    <h4 className="d-inline fw-bold car-head">{name} ({model})</h4>
+                </Link>
+            </div>
         </div>
     );
 };
