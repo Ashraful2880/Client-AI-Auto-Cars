d0373f58073bc42d7c5c1cc991b4f91db373b3b4
Ashraful <ashrafulislam2880@gmail.com>
2022-02-14 13:40:59 +0600

    Payment Successfull Page Done

--- src/Page/Dashboard/MyOrder/Success.js
+++ src/Page/Dashboard/MyOrder/Success.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Success = () => {
     return (
-        <div>
-            <h1>This is Success Page</h1>
+        <div className="rate-container pt-5 text-light">
+            <h1>Payment Successfull</h1>
+            <h3>Please Confirm Your Order</h3>
+            <button className="btn btn-success px-5 fs-5 mt-4">Confirm</button>
         </div>
     );
 };
